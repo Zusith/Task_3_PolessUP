7d3cca5d914164f6a88cad552846b040abb116fd
Kartech <kartech222@outlook.com>
2023-03-30 22:30:45 +0300

    Добавьте файлы проекта.

---
+++ Task_3_PolessUP/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ввод: ");
+        int[] nums = new int[] { 1, 3, 5, 6 };
+        int target = 5;
+        Array.Sort(nums);
+        Console.WriteLine("[" + string.Join(",", nums) + "]");
+        Console.WriteLine(target);
+        Console.WriteLine("Вывод: ");
+        Console.WriteLine(FindIndexElement(nums, target));
+        Console.ReadLine();
+    }
+
+    static int FindIndexElement(int[] nums, int target)
+    {
+        if (nums.Contains(target))
+        {
+             return Array.IndexOf(nums, target);
+        }
+        else
+        {
+            return FindIndexIfElementMissing(nums, target);
+        }
+    }
+
+    static int FindIndexIfElementMissing(int[] nums, int target)
+    {
+        for (int index = 0; index < nums.Length - 1; index++)
+        {
+            if (nums[index] < target && nums[index + 1] > target)
+            {
+                return index + 1;
+            }
+        }
+        return nums.Length;
+    }
+}
